9e59ae483e297760767ee58f12a60066667926a8
Jamie Magee <jamie.magee@gmail.com>
2025-08-24 14:58:38 -0700

    msbuild: 16.9.0 -> 17.14.8

--- pkgs/by-name/ms/msbuild/package.nix
+++ pkgs/by-name/ms/msbuild/package.nix
@@ -1,163 +1,132 @@
 {
   lib,
-  stdenv,
-  fetchurl,
-  makeWrapper,
-  glibcLocales,
-  mono,
-  unzip,
+  buildDotnetModule,
+  fetchFromGitHub,
   dotnetCorePackages,
-  roslyn,
+  stdenv,
 }:
 
-let
-
-  dotnet-sdk = dotnetCorePackages.sdk_6_0-bin;
-
-  xplat = fetchurl {
-    url = "https://github.com/mono/msbuild/releases/download/v16.9.0/mono_msbuild_6.12.0.137.zip";
-    sha256 = "1wnzbdpk4s9bmawlh359ak2b8zi0sgx1qvcjnvfncr1wsck53v7q";
-  };
-
-  inherit (stdenv.hostPlatform.extensions) sharedLibrary;
-
-  mkPackage =
-    attrs:
-    stdenv.mkDerivation (
-      finalAttrs:
-      dotnetCorePackages.addNuGetDeps {
-        nugetDeps = ./deps.json;
-        overrideFetchAttrs = a: {
-          dontBuild = false;
-        };
-      } attrs finalAttrs
-    );
-
-in
-
-mkPackage rec {
+buildDotnetModule rec {
   pname = "msbuild";
-  version = "16.10.1+xamarinxplat.2021.05.26.14.00";
+  version = "17.14.8";
 
-  src = fetchurl {
-    url = "https://download.mono-project.com/sources/msbuild/msbuild-${version}.tar.xz";
-    sha256 = "05ghqqkdj4s3d0xkp7mkdzjig5zj3k6ajx71j0g2wv6rdbvg6899";
+  src = fetchFromGitHub {
+    owner = "dotnet";
+    repo = "msbuild";
+    rev = "v${version}";
+    hash = "sha256-hi79m8FEXv8agRgg/gCe9AiNcu/jRy2sk74r9cvO1JY=";
   };
 
-  nativeBuildInputs = [
-    dotnet-sdk
-    mono
-    unzip
-    makeWrapper
+  nugetDeps = ./deps.json;
+
+  dotnet-sdk = dotnetCorePackages.sdk_9_0;
+  dotnet-runtime = dotnetCorePackages.runtime_9_0;
+
+  projectFile = [
+    "src/Framework/Microsoft.Build.Framework.csproj"
+    "src/Build/Microsoft.Build.csproj"
+    "src/Utilities/Microsoft.Build.Utilities.csproj"
+    "src/Tasks/Microsoft.Build.Tasks.csproj"
+  ];
+  buildType = "Release";
+
+  dotnetBuildFlags = [
+    "-p:DotNetBuildOffline=true"
+    "-p:DotNetBuildSourceOnly=true"
+    "-p:DisableImplicitNuGetFallbackFolder=true"
+    "-p:RepositoryCommit=unknown"
+    "-p:EnableApiCheck=false"
+    "-p:RunApiCompat=false"
+    "-p:IsPackable=false"
+    "-p:TargetFramework=net9.0"
+    "-p:UseAppHost=false"
+    "-p:PublishSingleFile=false"
+    "-p:SelfContained=false"
+    "-maxcpucount:1"
   ];
 
-  buildInputs = [
-    glibcLocales
+  dotnetInstallFlags = [
+    "-p:TargetFramework=net9.0"
   ];
-
-  # https://github.com/NixOS/nixpkgs/issues/38991
-  # bash: warning: setlocale: LC_ALL: cannot change locale (en_US.UTF-8)
-  LOCALE_ARCHIVE = lib.optionalString stdenv.hostPlatform.isLinux "${glibcLocales}/lib/locale/locale-archive";
 
   postPatch = ''
-    # not patchShebangs, there is /bin/bash in the body of the script as well
-    substituteInPlace ./eng/cibuild_bootstrapped_msbuild.sh --replace /bin/bash ${stdenv.shell}
+    # Fix executable permissions for shell scripts
+    chmod +x eng/cibuild_bootstrapped_msbuild.sh
+    chmod +x eng/common/*.sh
 
-    patchShebangs eng/*.sh mono/build/*.sh
+    # Substitute shell paths
+    substituteInPlace eng/cibuild_bootstrapped_msbuild.sh \
+      --replace '/bin/bash' '${stdenv.shell}'
 
-    sed -i -e "/<\/projectImportSearchPaths>/a <property name=\"MSBuildExtensionsPath\" value=\"$out/lib/mono/xbuild\"/>" \
-      src/MSBuild/app.config
+    find eng/ -name "*.sh" -exec sed -i 's|#!/bin/bash|#!${stdenv.shell}|g' {} \;
 
-    # license check is case sensitive
-    mv LICENSE license.bak && mv license.bak license
+    # Remove problematic AddRefAssemblies target that requires netstandard.library package
+    sed -i '/<Target Name="AddRefAssemblies"/,/<\/Target>/d' src/MSBuild/MSBuild.csproj
+
+    # Remove PackageDownload for netstandard.library since we're not using AddRefAssemblies
+    sed -i '/<PackageDownload Include="netstandard.library">/,/<\/PackageDownload>/d' src/MSBuild/MSBuild.csproj
+
+    # Remove GetCustomPackageFiles target that tries to use AddRefAssemblies output
+    sed -i '/<Target Name="GetCustomPackageFiles"/,/<\/Target>/d' src/MSBuild/MSBuild.csproj
   '';
 
-  linkNugetPackages = true;
+  postInstall = ''
+    # Build MSBuild CLI separately to avoid apphost conflicts
+    pushd src/MSBuild
 
-  buildPhase = ''
-    mkdir -p artifacts
-    unzip ${xplat} -d artifacts
-    mv artifacts/msbuild artifacts/mono-msbuild
-    chmod +x artifacts/mono-msbuild/MSBuild.dll
+    # First restore packages for MSBuild project - specify linux runtime to avoid Windows packages
+    dotnet restore MSBuild.csproj \
+      --packages ../../../nuget_pkgs \
+      --force-evaluate \
+      --no-cache \
+      -r linux-x64
 
-    # The provided libhostfxr.dylib is for x86_64-darwin, so we remove it
-    rm artifacts/mono-msbuild/SdkResolvers/Microsoft.DotNet.MSBuildSdkResolver/libhostfxr.dylib
+    dotnet build -c Release --no-restore -f net9.0 \
+      -p:DotNetBuildOffline=true \
+      -p:UseAppHost=false \
+      -p:PublishSingleFile=false \
+      -p:SelfContained=false \
+      -r linux-x64
 
-    ln -s $(find ${dotnet-sdk.unwrapped}/share/dotnet -name libhostfxr${sharedLibrary}) artifacts/mono-msbuild/SdkResolvers/Microsoft.DotNet.MSBuildSdkResolver/
+    # Copy MSBuild.dll and dependencies from artifacts directory
+    mkdir -p $out/lib/msbuild
+    if [ -f "../../artifacts/bin/MSBuild/Release/linux-x64/net9.0/MSBuild.dll" ]; then
+      cp -r ../../artifacts/bin/MSBuild/Release/linux-x64/net9.0/* $out/lib/msbuild/
+    elif [ -f "../../artifacts/bin/MSBuild/Release/net9.0/MSBuild.dll" ]; then
+      cp -r ../../artifacts/bin/MSBuild/Release/net9.0/* $out/lib/msbuild/
+    else
+      echo "Could not find MSBuild.dll in artifacts directory"
+      find ../../artifacts -name "MSBuild.dll" -type f
+      exit 1
+    fi
 
-    # overwrite the file
-    echo "#!${stdenv.shell}" > eng/common/dotnet-install.sh
-    echo "#!${stdenv.shell}" > mono/build/get_sdk_files.sh
+    # Create MSBuild wrapper script
+    mkdir -p $out/bin
+    cat > $out/bin/MSBuild << EOF
+#!/bin/bash
+exec "${dotnet-runtime}/bin/dotnet" "$out/lib/msbuild/MSBuild.dll" "\$@"
+EOF
+    chmod +x $out/bin/MSBuild
 
-    # Prevent msbuild from downloading a new libhostfxr
-    echo "#!${stdenv.shell}" > mono/build/extract_and_copy_hostfxr.sh
+    # Create lowercase wrapper for consistency with other build tools
+    ln -s $out/bin/MSBuild $out/bin/msbuild
 
-    mkdir -p mono/dotnet-overlay/msbuild-bin
-    cp ${dotnet-sdk.unwrapped}/share/dotnet/sdk/*/{Microsoft.NETCoreSdk.BundledVersions.props,RuntimeIdentifierGraph.json} mono/dotnet-overlay/msbuild-bin
-
-    # DisableNerdbankVersioning https://gitter.im/Microsoft/msbuild/archives/2018/06/27?at=5b33dbc4ce3b0f268d489bfa
-    # TODO there are some (many?) failing tests
-    NuGetPackageRoot="$NUGET_PACKAGES" ./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests /p:DisableNerdbankVersioning=true
-    patchShebangs stage1/mono-msbuild/msbuild
+    popd
   '';
 
-  installPhase = ''
-    stage1/mono-msbuild/msbuild mono/build/install.proj /p:MonoInstallPrefix="$out" /p:Configuration=Release-MONO
-
-    ln -s ${roslyn}/lib/dotnet/microsoft.net.compilers.toolset/*/tasks/net472 $out/lib/mono/msbuild/Current/bin/Roslyn
-
-    makeWrapper ${mono}/bin/mono $out/bin/msbuild \
-      --set-default MONO_GC_PARAMS "nursery-size=64m" \
-      --add-flags "$out/lib/mono/msbuild/15.0/bin/MSBuild.dll"
-
-    ln -s $(find ${dotnet-sdk.unwrapped}/share/dotnet -name libhostfxr${sharedLibrary}) $out/lib/mono/msbuild/Current/bin/SdkResolvers/Microsoft.DotNet.MSBuildSdkResolver/
-  '';
-
-  doInstallCheck = true;
-
-  # https://docs.microsoft.com/cs-cz/visualstudio/msbuild/walkthrough-creating-an-msbuild-project-file-from-scratch?view=vs-2019
-  installCheckPhase = ''
-        cat > Helloworld.cs <<EOF
-    using System;
-
-    class HelloWorld
-    {
-        static void Main()
-        {
-    #if DebugConfig
-            Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
-    #endif
-
-            Console.WriteLine("Hello, world!");
-        }
-    }
-    EOF
-
-        cat > Helloworld.csproj <<EOF
-    <Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-      <ItemGroup>
-        <Compile Include="Helloworld.cs" />
-      </ItemGroup>
-      <Target Name="Build">
-        <Csc Sources="@(Compile)"/>
-      </Target>
-    </Project>
-    EOF
-
-        $out/bin/msbuild Helloworld.csproj -t:Build
-        ${mono}/bin/mono Helloworld.exe | grep "Hello, world!"
-  '';
+  # Disable tests as they require specific Microsoft testing infrastructure
+  doCheck = false;
 
   meta = with lib; {
-    description = "Mono version of Microsoft Build Engine, the build platform for .NET, and Visual Studio";
+    description = "Microsoft Build Engine (MSBuild) - the build platform for .NET and Visual Studio";
     mainProgram = "msbuild";
-    homepage = "https://github.com/mono/msbuild";
+    homepage = "https://github.com/dotnet/msbuild";
+    changelog = "https://github.com/dotnet/msbuild/releases/tag/v${version}";
     sourceProvenance = with sourceTypes; [
       fromSource
-      binaryNativeCode # dependencies
     ];
     license = licenses.mit;
     maintainers = with maintainers; [ jdanek ];
-    platforms = platforms.unix;
+    platforms = platforms.linux ++ platforms.darwin;
   };
 }
